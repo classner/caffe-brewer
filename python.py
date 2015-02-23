a1b4e4852cb0bb8985a5a9e9ad2e061c002dac7c
Christoph Lassner <mail@christophlassner.de>
2015-02-23 07:02:49 +0100

    Fixed python linking issue.

--- python.py
+++ python.py
@@ -11,7 +11,15 @@ if py_env['CC'] == 'cl':
   # Set warning level to 3 (highest, before all warnings).
   py_env.AppendUnique(CPPFLAGS='/W3')
 file_list = Glob('caffe-framework/python/caffe/_caffe.cpp')
-py_env.PrependUnique(LIBS=link_libs)
+# It is necessary to circumvent the automatic symbol removal from the
+# static library.
+if os.name != 'nt':
+  py_env.PrependUnique(LIBS=link_libs[1:])
+  py_env['LINKFLAGS']='-Wl,--whole-archive %s -Wl,--no-whole-archive' % (str(link_libs[0][0]))
+else:
+  py_env.PrependUnique(LIBS=link_libs)
 py_module = py_env.SharedLibrary(file_list)
+if os.name != 'nt':
+  py_env.Depends(py_module, link_libs[0])
 # The module.
 Return("py_module")
